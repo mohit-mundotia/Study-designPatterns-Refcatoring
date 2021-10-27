ebbfbcfcb2517a11bbf23c7ecdf172cd521d384e
Mohit Mundotia <84668881+mohit-mundotia@users.noreply.github.com>
2021-10-27 10:44:15 +0530

    Add files via upload

---
+++ Design Patterns/singleton/SingletonExample.java
@@ -0,0 +1,11 @@
+public class SingletonExample {
+
+    public static void main(String[] args) {
+
+        Singleton instance = Singleton.getInstance();
+        System.out.println(instance);
+        Singleton secondInstance = Singleton.getInstance();
+        System.out.println(secondInstance);
+
+    }
+}
